54cd0e1940d0c2ab2f64aa1c9dc9faa0c79a6885
takeuchi <oca2204240010@edu.oca.ac.jp>
2024-06-22 15:19:44 +0900

    [update] new route '/test_request' added.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 @app.route('/')
@@ -7,3 +8,6 @@ def inex():
 @app.route('/another')
 def index():
     return 'Another Response'
+@app.route('/test_request')
+def test_request():
+    return f'test_request:{request.args.get("dummy")}'
