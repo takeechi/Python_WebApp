1788cc5948b64b364fd08fe84d4c04ec651496fa
takeuchi <oca2204240010@edu.oca.ac.jp>
2024-09-28 14:44:28 +0900

    [add]sqlalchemy用のテストファイルを作成

--- test_model.py
+++ test_model.py
@@ -1,23 +1,13 @@
-from sqlalchemy import Column, Float, Integer, Table, Text
+from sqlalchemy import Column, Float, Integer, Text
 from sqlalchemy.orm import Mapped, declarative_base, mapped_column
 from sqlalchemy.orm.base import Mapped
 
 Base = declarative_base()
-metadata = Base.metadata
 
 
-class Human(Base):
-    __tablename__ = 'human'
+class Person(Base):
+    __tablename__ = 'person'
 
     name = mapped_column(Text, nullable=False)
     id = mapped_column(Integer, primary_key=True)
-    height = mapped_column(Float)
-    weight = mapped_column(Float)
-
-
-t_person = Table(
-    'person', metadata,
-    Column('id', Integer),
-    Column('name', Text, nullable=False),
-    Column('size', Float)
-)
+    size = mapped_column(Float)
